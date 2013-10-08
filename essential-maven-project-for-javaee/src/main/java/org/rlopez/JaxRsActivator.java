d902ac56c91ee751822e51985b078126c8796ea9
Ruben Lopez <rlopez@konecta.com.py>
2013-10-07 23:41:29 -0300

    Essential configuration files.

---
+++ essential-maven-project-for-javaee/src/main/java/org/rlopez/JaxRsActivator.java
@@ -0,0 +1,12 @@
+package org.rlopez;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author rlopez
+ *
+ */
+@ApplicationPath("/v1")
+public class JaxRsActivator extends Application {
+}
---
+++ essential-maven-project-for-javaee/src/main/java/org/rlopez/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author rlopez
+ *
+ */
+package org.rlopez;
